c5b9dcecdf4549dfd87f807902466bff0f2cbbee
dienoov <alvinodien@yahoo.co.id>
2022-11-05 09:16:56 +0700

    Convert class component to functional component

--- src/components/note/NewNote.jsx
+++ src/components/note/NewNote.jsx
@@ -1,124 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-class NewNote extends React.Component {
-  static get initialStates() {
-    return {
-      title: '',
-      body: '',
-      titleLimit: '50/50',
-      show: false,
-      errors: [],
-    };
-  }
+function NewNote({ onSave }) {
+  const LIMIT = 50;
 
-  constructor(props) {
-    super(props);
+  const initialStates = {
+    title: '',
+    body: '',
+    titleLimit: `${LIMIT}/${LIMIT}`,
+    show: false,
+    errors: [],
+  };
 
-    this.state = NewNote.initialStates;
+  const [title, setTitle] = useState(initialStates.title);
+  const [body, setBody] = useState(initialStates.body);
+  const [titleLimit, setTitleLimit] = useState(initialStates.titleLimit);
+  const [show, setShow] = useState(initialStates.show);
+  const [errors, setErrors] = useState(initialStates.errors);
 
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onBodyChange = this.onBodyChange.bind(this);
-    this.onCancel = this.onCancel.bind(this);
-    this.onShow = this.onShow.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-  }
+  const resetToInitialState = () => {
+    setTitle(initialStates.title);
+    setBody(initialStates.body);
+    setTitleLimit(initialStates.titleLimit);
+    setShow(initialStates.show);
+    setErrors(initialStates.errors);
+    document.getElementById('input-body').innerHTML = '';
+  };
 
-  onTitleChange(ev) {
-    const title = ev.target.value.substring(0, 50);
-    const limit = 50 - title.length;
+  const onTitleChange = (ev) => {
+    setTitle(ev.target.value.substring(0, 50));
+    setTitleLimit(`${50 - ev.target.value.substring(0, 50).length}/${LIMIT}`);
+  };
 
-    this.setState((prev) => ({
-      ...prev,
-      title,
-      titleLimit: `${limit}/50`,
-    }));
-  }
+  const onBodyChange = (ev) => {
+    setBody(ev.target.innerHTML);
+  };
 
-  onBodyChange(ev) {
-    this.setState((prev) => ({
-      ...prev,
-      body: ev.target.innerHTML,
-    }));
-  }
+  const onCancel = () => {
+    resetToInitialState();
+  };
 
-  onCancel() {
-    this.setState(({
-      ...NewNote.initialStates,
-    }));
-  }
+  const onShow = () => {
+    resetToInitialState();
+    setShow(true);
+  };
 
-  onShow() {
-    this.setState(({
-      ...NewNote.initialStates,
-      show: true,
-    }));
-  }
-
-  onSubmit(ev) {
+  const onSubmit = (ev) => {
     ev.preventDefault();
 
-    const { title, body } = this.state;
-    const errors = [];
+    const newErrors = [];
 
-    if (title.length === 0) errors.push('Title must not be empty');
-    if (title.length > 50) errors.push('Title must be less than 50 characters');
-    if (body.length === 0) errors.push('Body must not be empty');
+    if (title.length === 0) newErrors.push('Title must not be empty');
+    if (title.length > 50) newErrors.push('Title must be less than 50 characters');
+    if (body.length === 0) newErrors.push('Body must not be empty');
 
     if (errors.length !== 0) {
-      this.setState((prev) => ({
-        ...prev,
-        errors,
-      }));
+      setErrors(newErrors);
       return;
     }
 
-    const { onSave } = this.props;
     onSave({ title, body });
 
-    this.setState({
-      ...NewNote.initialStates,
-    });
-  }
+    resetToInitialState();
+  };
 
-  render() {
-    const {
-      title, titleLimit, show, errors,
-    } = this.state;
-
-    return (
-      <>
-        <div className="fab">
-          <button type="button" className="fab__modal" onClick={this.onShow}>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2}>
-              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-            </svg>
-            <span>New Note</span>
-          </button>
-        </div>
-        <div className={`modal ${(show ? 'show' : '')}`}>
-          <form action="#" className="modal__form" onSubmit={this.onSubmit}>
-            <label htmlFor="input-title" className="modal__input">
-              <input type="text" id="input-title" placeholder="Title" value={title} onChange={this.onTitleChange} />
-              <span className="modal__limit">{titleLimit}</span>
-            </label>
-            <div className="modal__input">
-              <div id="input-body" data-placeholder="Write something here..." onInput={this.onBodyChange} contentEditable />
-            </div>
-            <ul className="modal__errors">
-              {errors.map((error) => (
-                <li>{error}</li>
-              ))}
-            </ul>
-            <div className="modal__actions">
-              <button type="button" className="modal__cancel" onClick={this.onCancel}>Cancel</button>
-              <button type="submit" className="modal__submit">Save</button>
-            </div>
-          </form>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      <div className="fab">
+        <button type="button" className="fab__modal" onClick={onShow}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2}>
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+          </svg>
+          <span>New Note</span>
+        </button>
+      </div>
+      <div className={`modal ${(show ? 'show' : '')}`}>
+        <form action="#" className="modal__form" onSubmit={onSubmit}>
+          <label htmlFor="input-title" className="modal__input">
+            <input type="text" id="input-title" placeholder="Title" value={title} onChange={onTitleChange} />
+            <span className="modal__limit">{titleLimit}</span>
+          </label>
+          <div className="modal__input">
+            <div id="input-body" data-placeholder="Write something here..." onInput={onBodyChange} contentEditable />
+          </div>
+          <ul className="modal__errors">
+            {errors.map((error) => (
+              <li>{error}</li>
+            ))}
+          </ul>
+          <div className="modal__actions">
+            <button type="button" className="modal__cancel" onClick={onCancel}>Cancel</button>
+            <button type="submit" className="modal__submit">Save</button>
+          </div>
+        </form>
+      </div>
+    </>
+  );
 }
 
 NewNote.propTypes = {
